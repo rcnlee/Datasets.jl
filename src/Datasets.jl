6af8dc237d0a5ed44bf962c88bda6fc51e9a2522
Ritchie Lee <ritchie.lee@sv.cmu.edu>
2016-01-04 20:05:01 -0800

    support for loading as a set

--- src/Datasets.jl
+++ src/Datasets.jl
@@ -1,12 +1,14 @@
-#Excerpted from RDatasets
+#Adopted from RDatasets
 
 module Datasets
 
 using Reexport
 @reexport using DataFrames
+@reexport using DataFrameSets
 
 export dataset
 
+#Load a particular dataframe
 function dataset(package_name::AbstractString, dataset_name::AbstractString)
     basename = joinpath(dirname(@__FILE__), "..", "data", package_name)
 
@@ -17,5 +19,21 @@ function dataset(package_name::AbstractString, dataset_name::AbstractString)
         return readtable(filename)
     end
 end
+
+#Load all dataframes in the package, returns a DFSet
+function dataset(package_name::AbstractString; getname::Function=fileroot)
+  dir = joinpath(dirname(@__FILE__), "..", "data", package_name)
+  files = readdir(dir)
+
+  dfs = DFSet()
+  for f in files
+    D = readtable(joinpath(dir, f))
+    name = getname(f, D)
+    push!(dfs, (name, D))
+  end
+  return dfs
+end
+
+fileroot(filename::AbstractString, D::DataFrame) = split(basename(filename), '.')[1]
 
 end # module
